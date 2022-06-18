93e083af62b7edd1c52dc9ff2ae2a697f30e19dd
geraskalnas <geraskalnas@gmail.com>
2022-06-18 23:14:55 +0300

    mouse functions on client

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
-from pynput import keyboard
+from pynput import keyboard, mouse
 import requests
 from lib import key_to_special
 from time import sleep
 
 ADDRESS = "http://10.42.0.92:5000/"
+
+
+#KEYBOARD
 
 def on_key_press(key):
     try:
@@ -28,12 +31,46 @@ def on_key_release(key):
         # Stop listener
         return False
 
-# Collect events until released
+
+#MOUSE
+
+def on_mouse_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+    requests.get(ADDRESS + "mouse/move", params={"x": x, "y": y})
+
+def on_button_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+
+    if(pressed):
+        requests.get(ADDRESS + "mouse/press", params={"x": x, "y": y})
+    else:
+        requests.get(ADDRESS + "mouse/release", params={"x": x, "y": y})
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format(
+        'down' if dy < 0 else 'up',
+        (x, y)))
+    requests.get(ADDRESS + "mouse/scroll", params={"x": x, "y": y, "dx": dx, "dy": dy})
+
+
+
+#LISTENERS
+
 k_listener = keyboard.Listener(
     on_press=on_key_press,
     on_release=on_key_release)
 k_listener.start()
 
+m_listener = mouse.Listener(
+    on_move=on_mouse_move,
+    on_click=on_button_click,
+    on_scroll=on_scroll)
+m_listener.start()
+
 while True:
     sleep(0.1)
+    if(not k_listener.running): exit()
     if(not k_listener.running): exit()
