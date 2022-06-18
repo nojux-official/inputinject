bd7eedea42ff8abf0865f577c28ec61d80b7e50a
geraskalnas <geraskalnas@gmail.com>
2022-06-18 16:02:14 +0300

    move keyboard urls to blueprint

--- server.py
+++ server.py
@@ -1,10 +1,5 @@
-from pynput.keyboard import Controller
-from flask import Flask, request
-from lib import special_to_key
-
-
-keyboard = Controller()
-
+from flask import Flask
+import routes.keyboard
 
 app = Flask(__name__)
 
@@ -18,52 +13,8 @@ def index():
     return "Ready!"
 
 
-# KEYBOARD
-
-def get_key():
-    try:
-        is_special = True if request.args.get("special") == "true" else False
-    except Exception:
-        is_special = False
-    
-    key = request.args.get('key')
-    
-    if(is_special): key = special_to_key(key)
-
-    return key
-
-
-@app.route('/press')
-def press():
-    key = get_key()
-
-    keyboard.press(key)
-    return '{"status": "OK"}'
-
-@app.route('/release')
-def release():
-    key = get_key()
-
-    keyboard.release(key)
-    return '{"status": "OK"}'
-
-@app.route('/pr')
-def pr():
-    key = get_key()
-    
-    keyboard.press(key)
-    keyboard.release(key)
-    return '{"status": "OK"}'
-
-@app.route('/type')
-def type():
-    try:
-        text = request.args.get('text')
-    except Exception:
-        return '{"status": "Error", "error": "Missing \\"type\\" parameter."}'
-
-    keyboard.type(text)
-    return '{"status": "OK"}'
+# KEYBOAR
+app.register_blueprint(routes.keyboard.hub)
 
 # MOUSE
 
