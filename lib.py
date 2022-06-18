6b01873ab285e1bb79a45ca4a12ab9e8a84cb6f9
geraskalnas <geraskalnas@gmail.com>
2022-06-18 21:27:22 +0300

    add mouse button recognizing functions to lib

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 from pynput.keyboard import Key
+from pynput.mouse import Button
 
 def special_to_key(special):
     try:
@@ -9,5 +10,17 @@ def special_to_key(special):
 def key_to_special(key):
     for k, v in Key.__members__.items():
         if(v == key):
+            return k
+    return None
+
+def special_to_button(special):
+    try:
+        return Button[special]
+    except Exception:
+        return None
+
+def button_to_special(button):
+    for k, v in Button.__members__.items():
+        if(v == button):
             return k
     return None
